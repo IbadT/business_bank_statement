8a001501a2007b20a76f7163d39573e2a62b975f
Eduard Kononovich <101503726+IbadT@users.noreply.github.com>
2025-10-06 13:28:09 +0300

    fix lint

---
+++ src/pages/HomePage/constants.ts
@@ -0,0 +1,31 @@
+export interface BusinessBankStatement {
+    id: number;
+    text: string;
+}
+
+export const businessBankStatementList: BusinessBankStatement[] = [
+    {
+        id: 1,
+        text: " Company Details"
+    },
+    {
+        id: 2,
+        text: " Financial and Owners"
+    },
+    {
+        id: 3,
+        text: "Clients & Payment Gateways"
+    },
+    {
+        id: 4,
+        text: "Contractors"
+    },
+    {
+        id: 5,
+        text: "Top-ups & Expenses"
+    },
+    {
+        id: 6,
+        text: "Review & Checkout"
+    }
+]
